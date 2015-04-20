fac5f2dfbe386fb4692ed683c75ea371c2f7d904
VictorMarcal <victormarcal90@hotmail.com>
2015-04-20 21:34:58 +0100

    class camera/sprite and scene

---
+++ TDJ_ProjectoFinal/TDJ_ProjectoFinal/Game1.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using TDJ_ProjectoFinal.graficos;
+
+namespace TDJ_ProjectoFinal
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Scene scene;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.PreferredBackBufferWidth = 1000;
+            graphics.ApplyChanges();
+
+            Camera.SetGraphicsDeviceManager(graphics);
+            Camera.SetTarget(Vector2.Zero);
+            Camera.SetWorldWidth(5);
+
+           
+            base.Initialize();
+        }
+
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            scene = new Scene(spriteBatch);
+            scene.AddSprite(new Sprite(Content, "universe").Scl(Camera.worldWidth).
+                At(new Vector2(0f, 5000 * Camera.worldWidth / 600)));
+            
+        }
+
+            
+
+        protected override void UnloadContent()
+        {
+            
+        }
+
+       
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            scene.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+      
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+           
+
+            scene.Draw(gameTime);
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TDJ_ProjectoFinal/TDJ_ProjectoFinal/graficos/Scene.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TDJ_ProjectoFinal.graficos;
+
+namespace TDJ_ProjectoFinal
+{
+    class Scene
+    {
+        public SpriteBatch SpriteBatch { get; private set; }
+        private List<Sprite> sprites;
+
+        public Scene(SpriteBatch sb)
+        {
+            this.SpriteBatch = sb;
+            this.sprites = new List<Sprite>();
+        }
+
+        public void AddSprite(Sprite s)
+        {
+            this.sprites.Add(s);
+            s.SetScene(this);
+        }
+
+        public void RemoveSprite(Sprite s)
+        {
+            this.sprites.Remove(s);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            foreach (var sprite in sprites.ToList())
+            {
+                sprite.Update(gameTime);
+            }
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            if (sprites.Count > 0)
+            {
+                this.SpriteBatch.Begin();
+                foreach (var sprite in sprites)
+                {
+                    sprite.Draw(gameTime);
+                }
+                this.SpriteBatch.End();
+            }
+        }
+
+        public bool Collides(Sprite s, out Sprite collided,
+                                       out Vector2 collisionPoint)
+        {
+            bool collisionExists = false;
+            collided = s;  // para calar o compilador
+            collisionPoint = Vector2.Zero; // para calar o compilador
+
+            foreach (var sprite in sprites)
+            {
+                if (s == sprite) continue;
+                if (s.CollidesWith(sprite, out collisionPoint))
+                {
+                    collisionExists = true;
+                    collided = sprite;
+                    break;
+                }
+            }
+            return collisionExists;
+        }
+
+        public void Dispose()
+        {
+            foreach (var sprite in sprites)
+            {
+                sprite.Dispose();
+            }
+        }
+    }
+}
