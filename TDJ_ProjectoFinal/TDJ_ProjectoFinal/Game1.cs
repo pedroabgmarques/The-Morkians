0f2eea5ce3c5e967dd8da4d0466fa60ba54100c5
paulodgn <paulo.nwork@gmail.com>
2015-04-23 01:34:58 +0100

    Classe Power Up. Inseri um power up no jogo. Criação de enimigos em Game1 passou para metodo NewEnemyWave.

--- TDJ_ProjectoFinal/TDJ_ProjectoFinal/Game1.cs
+++ TDJ_ProjectoFinal/TDJ_ProjectoFinal/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 using TDJ_ProjectoFinal.entidades;
 using TDJ_ProjectoFinal.graficos;
 
@@ -14,7 +15,8 @@ namespace TDJ_ProjectoFinal
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Scene scene;
-
+        
+        
         public Game1()
             : base()
         {
@@ -32,6 +34,8 @@ namespace TDJ_ProjectoFinal
             Camera.SetTarget(Vector2.Zero);
             Camera.SetWorldWidth(5);
 
+            
+            
            
             base.Initialize();
         }
@@ -41,13 +45,57 @@ namespace TDJ_ProjectoFinal
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
             scene = new Scene(spriteBatch);
-
+            
             //Fundo do universo (imóvel)
             scene.AddSprite(new Sprite(Content, "universe").Scl(6000 * Camera.worldWidth / graphics.PreferredBackBufferHeight).
                 At(new Vector2(Camera.worldWidth, 0f)));
             
             //Nave do jogador
             scene.AddSprite(new Player(Content, "nave").Scl(0.5f));
+
+            newEnemyWave();
+         
+        }
+
+            
+
+        protected override void UnloadContent()
+        {
+            
+        }
+
+       
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            scene.Update(gameTime);
+
+            Camera.Update();
+
+          
+
+            scene.Update(gameTime);
+            
+            base.Update(gameTime);
+        }
+
+      
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+           
+
+            scene.Draw(gameTime);
+
+            base.Draw(gameTime);
+        }
+
+    
+        public void newEnemyWave()
+        {
 
             //Alguns inimigos
             scene.AddSprite(new NPC(Content, "nave", TipoNave.Hunter).Scl(0.5f).
@@ -70,41 +118,9 @@ namespace TDJ_ProjectoFinal
             //Missil
             scene.AddSprite(new Missil(Content, "missil", TipoMissil.EmFrente, -1).Scl(0.2f).
                 At(new Vector2(Camera.worldWidth, 0f)));
-         
-        }
 
-            
-
-        protected override void UnloadContent()
-        {
-            
-        }
-
-       
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            scene.Update(gameTime);
-
-            Camera.Update();
-
-            scene.Update(gameTime);
-            
-            base.Update(gameTime);
-        }
-
-      
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-           
-
-            scene.Draw(gameTime);
-
-            base.Draw(gameTime);
+            //PowerUP
+            scene.AddSprite(new PowerUp(Content, "PowerUp-Vida", -1).Scl(0.3f).At(new Vector2(Camera.worldWidth/2, 0f)));
         }
     }
 }
---
+++ TDJ_ProjectoFinal/TDJ_ProjectoFinal/entidades/PowerUp.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDJ_ProjectoFinal.entidades
+{
+    class PowerUp : FlyingEntity
+    {
+        private int direcao;
+        private float speed;
+        public PowerUp(ContentManager contents, string assetName, int direcao) : base(contents, assetName)
+        {
+            base.spriteEffects = direcao > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+            this.direcao = direcao;
+            this.speed = Camera.speed * 0.5f;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.position.X += speed * direcao;
+        }
+
+    }
+}
