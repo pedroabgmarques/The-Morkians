e550cd9c54d352a2cc36c3a73746e10e3c5230c1
pedroabgmarques <pedroabgmarques@gmail.com>
2015-06-01 23:19:00 +0100

    Resolução de conflitos

--- TDJ_ProjectoFinal/TDJ_ProjectoFinal/entidades/UI.cs
+++ TDJ_ProjectoFinal/TDJ_ProjectoFinal/entidades/UI.cs
@@ -9,25 +9,13 @@ using TDJ_ProjectoFinal.graficos;
 namespace TDJ_ProjectoFinal.entidades
 {
     /// <summary>
-<<<<<<< HEAD
-    /// Clasee UI
-=======
     /// Elementos de UI, fixos no ecrã
->>>>>>> 01b50400078fc77e05e33bb05b959d284833fed0
     /// </summary>
     public class UI : Sprite
     {
 
         Vector2 posicao;
         Vector2 offset;
-<<<<<<< HEAD
-        /// <summary>
-        /// Construtor
-        /// </summary>
-        /// <param name="content">Instancia de ContentManager</param>
-        /// <param name="assetName">Nome da sprite na pasta Contents</param>
-        /// <param name="offset">Ponto de offset</param>
-=======
 
         /// <summary>
         /// Construtor do elemento de UI
@@ -35,24 +23,17 @@ namespace TDJ_ProjectoFinal.entidades
         /// <param name="content">Instância de ContentManager</param>
         /// <param name="assetName">Nome da textura a utilizar</param>
         /// <param name="offset">Offset em relação à posição por defeito das UI</param>/param>
->>>>>>> 01b50400078fc77e05e33bb05b959d284833fed0
         public UI(ContentManager content, string assetName, Vector2 offset)
             : base(content, assetName)
         {
             this.offset = offset;
         }
-<<<<<<< HEAD
-        /// <summary>
-        /// Update
-        /// </summary>
-        /// <param name="gameTime">Instancia de gametime</param>
-=======
+
 
         /// <summary>
         /// Atualiza os componentes de UI
         /// </summary>
         /// <param name="gameTime">Instância de gameTime</param>
->>>>>>> 01b50400078fc77e05e33bb05b959d284833fed0
         public override void Update(GameTime gameTime)
         {
             posicao.X = Camera.GetTarget().X - Camera.worldWidth / 2 + 0.2f + offset.X;
