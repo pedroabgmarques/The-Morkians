eb27c0b1ffa44310bdded2293d7790a21e10aac6
pedroabgmarques <pedroabgmarques@gmail.com>
2015-06-01 23:02:45 +0100

    Documentação classe WeaponsManager

--- TDJ_ProjectoFinal/TDJ_ProjectoFinal/entidades/WeaponsManager.cs
+++ TDJ_ProjectoFinal/TDJ_ProjectoFinal/entidades/WeaponsManager.cs
@@ -8,13 +8,28 @@ using System.Text;
 
 namespace TDJ_ProjectoFinal.entidades
 {
+    /// <summary>
+    /// Faz a gestão de tiros e mísseis.
+    /// Aquando do Load são criadas centenas de instâncias destes objetos, que são recicladas de modo a evitar 
+    /// criar objetos em runtime.
+    /// </summary>
     public static class WeaponsManager
     {
-
+        /// <summary>
+        /// Lista de balas que estão neste momento ativas no jogo
+        /// </summary>
         public static List<Bala> balasAtivas;
+        /// <summary>
+        /// Lista de balas à espera de serem usadas
+        /// </summary>
         public static List<Bala> balasMortas;
-
+        /// <summary>
+        /// Lista de misseis ativos no jogo
+        /// </summary>
         public static List<Missil> misseisAtivos;
+        /// <summary>
+        /// Lista de mísses à espera de serem usados
+        /// </summary>
         public static List<Missil> misseisMortos;
 
         private static Bala balaTemp;
@@ -25,6 +40,10 @@ namespace TDJ_ProjectoFinal.entidades
 
         private static Vector2 vectorTemp;
 
+        /// <summary>
+        /// Carrega os assets necessários e cria centenas de instâncias
+        /// </summary>
+        /// <param name="contents">Instância de ContentManager</param>
         public static void LoadContent(ContentManager contents)
         {
 
@@ -59,6 +78,15 @@ namespace TDJ_ProjectoFinal.entidades
             }
         }
 
+        /// <summary>
+        /// Recicla um missil que é necessário para o jogo
+        /// </summary>
+        /// <param name="assetname">Textura do missil a utilizar</param>
+        /// <param name="tipoMissil">Tipo de missil - inimigo ou jogador</param>
+        /// <param name="direcao">Direção do ecrã em que o missil se move</param>
+        /// <param name="origemBala">Entidade que originou o missil</param>
+        /// <param name="alvo">Alvo do missil teleguiado</param>
+        /// <returns>Missil reciclado</returns>
         public static Missil addMissil(string assetname, TipoMissil tipoMissil, int direcao, OrigemBala origemBala, FlyingEntity alvo = null)
         {
             missilTemp = misseisMortos.First();
@@ -84,12 +112,25 @@ namespace TDJ_ProjectoFinal.entidades
             return missilTemp;
         }
 
+        /// <summary>
+        /// Recicla um missil que já não é necessário
+        /// </summary>
+        /// <param name="missil">Instância de missil a remover dos misseis ativos</param>
         public static void removeMissil(Missil missil)
         {
             misseisAtivos.Remove(missil);
             misseisMortos.Add(missil);
         }
 
+        /// <summary>
+        /// Recicla uma bala que é necessária para o jogo
+        /// </summary>
+        /// <param name="assetname">Nome da textura a utilizar</param>
+        /// <param name="direcao">Direção do ecrã em que a bala se movimenta</param>
+        /// <param name="origemBala">Entidade que originou a bala</param>
+        /// <param name="direcaoBala">Tipo de bala - Em frente, cima ou baixo</param>
+        /// <param name="parent">Turret que disparou a bala</param>
+        /// <returns>Uma bala reciclada</returns>
         public static Bala addBala(string assetname, int direcao, OrigemBala origemBala, DireccaoBala direcaoBala, Defence parent = null)
         {
             balaTemp = balasMortas.First();
@@ -121,6 +162,10 @@ namespace TDJ_ProjectoFinal.entidades
             return balaTemp;
         }
 
+        /// <summary>
+        /// Recicla uma bala que já não é necessária no jogo
+        /// </summary>
+        /// <param name="bala">Instância de bala a reciclar</param>
         public static void removeBala(Bala bala)
         {
 
